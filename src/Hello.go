016c1a891199d4cdddb1fbe2a8088aa0d8019b0c
Yile Liu <yile.liu@autodesk.com>
2017-02-27 22:13:28 +0800

    channel

--- src/Hello.go
+++ src/Hello.go
@@ -45,7 +45,33 @@ func main() {
 	greeting.TypeSwitchTest("Bob")
 
 	<- done
+	
+	c := make(chan greeting.Salutation)
+	c2 := make(chan greeting.Salutation)
+	go salutations.ChannelGreeter(c);
+	go salutations.ChannelGreeter(c2);
 
+	for {
+		select {
+			case s, ok := <- c: 
+				if ok {
+					fmt.Println(s, ":1")
+				}else{
+					return
+				}
+			case s, ok := <- c2: 
+				if ok {
+					fmt.Println(s, ":2")
+				}else{
+					return
+				}
+			
+
+			default:
+				fmt.Println("Waiting...")
+
+		}
+	}
 }
 
 
